2273f06a4562e50bdf5e5c08cc085e8158810cb7
citlalli-rvr <crivera14@ucol.mx>
2025-11-07 09:07:34 -0600

    Update asientos.php

--- asientos.php
+++ asientos.php
@@ -177,6 +177,7 @@ try {
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Selección de asientos — Reserva</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container mt-4">
@@ -309,3 +310,4 @@ try {
 </body>
 
 </html>
+
