a8cf31a20789f8f91539eb0a3ac61f1a734fc176
Yeray Anguiano <janguiano27@ucol.mx>
2025-11-07 08:15:09 -0600

    Fix button label and HTML closing tag

--- registro.php
+++ registro.php
@@ -214,7 +214,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['reservar_submit'])) {
                   <svg width="16" height="16" fill="currentColor" class="me-1">
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                   </svg>
-                  + Agregar Pasajero
+                  Agregar Pasajero
                 </button>
                 <small class="d-block text-muted mt-1">Máximo 11 pasajeros en total</small>
               </div>
